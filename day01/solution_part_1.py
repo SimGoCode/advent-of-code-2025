677d0f2c62a9f38636e841b51f3cc8872450c2e3
Simon Gouin <srgou3@ulaval.ca>
2025-12-03 15:35:08 -0500

    Day 1 Solutions

---
+++ day01/solution_part_1.py
@@ -0,0 +1,26 @@
+from input import INPUT
+
+def solve(input):
+    current_digit = 50
+    password = 0
+
+    for instruction in input.splitlines():
+
+        turn_left = instruction.startswith("L")
+        instruction = instruction[1:]
+
+        if turn_left:
+            current_digit = (current_digit - int(instruction)) % 100
+        else:
+            current_digit = (current_digit + int(instruction)) % 100
+
+        if current_digit == 0:
+            password+=1
+
+    return password
+
+
+if __name__ == "__main__":
+    result = solve(INPUT)
+    print("Result:", result)
+
---
+++ day01/solution_part_2.py
@@ -0,0 +1,41 @@
+from input import INPUT as input_data
+
+def solve(input):
+    current_digit = 50
+    password = 0
+
+    for instruction in input.splitlines():
+        turn_left = instruction.startswith("L")
+        step = int(instruction[1:])
+
+        # reducing the step from complete loops
+        password += step // 100
+        step = step % 100
+
+        if step != 0:
+
+            # If you start on 0, it's impossible to cross 0 again since we reduced the step
+            # so we just move the dial
+            if current_digit == 0:
+                if turn_left:
+                    current_digit = (current_digit - step) % 100
+                else:
+                    current_digit = (current_digit + step) % 100
+
+            else:
+                if turn_left:
+                    if step >= current_digit:
+                        password +=1
+                    current_digit = (current_digit - step) % 100
+
+                else:
+                    if step >= 100-current_digit:
+                        password += 1
+                    current_digit = (current_digit + step) % 100
+    return password
+
+
+if __name__ == "__main__":
+    result = solve(input_data)
+    print("Result:", result)
+
